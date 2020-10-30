cc6334e5e9bd4d749e9cbc5eec77e0f6a286a5c9
lizziehosler <lizzie.hosler@gmail.com>
2020-10-30 10:33:01 -0500

    final edits

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,5 @@ class PagesController < ApplicationController
     def show
       render template: "pages/#{params[:page]}"
     end
-
     
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require_tree .
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+
 
 
 import "bootstrap"
