a976aa3483fb00627c5c25af3fc7bae4da4364b7
lizziehosler <lizzie.hosler@gmail.com>
2020-11-05 16:22:11 -0600

    error setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
 
     def show
-      render template: "pages/#{params[:page]}"
+      if valid_page?
+        render template: "pages/#{params[:page]}"
+      else
+        render file: "public/404.html", status: :not_found 
+      end
     end
+
+    private
+    def valid_page?
+      File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
+    end
+
     def download 
       send_file Rails.root.join('public', 'Elizabeth_Hosler_Resume.pdf'), 
       :type=>"application/pdf", 
