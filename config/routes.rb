1c8d5998f05a1c3ee25a5c65532b0236d1dd94e1
lizziehosler <lizzie.hosler@gmail.com>
2021-01-03 18:49:23 -0600

    add video

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
-  root "pages#show", page: "home"
+  root "pages#show", page: "index"
   get 'download', to: "pages#download"
 end
